eadb4b19251d51d1e597653fc32c27f10d0d757d
PedroJQL <pedrozx368@gmail.com>
2024-05-07 23:17:13 -0600

    cambios responsive

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     var boton = document.getElementById('miBoton');
     boton.addEventListener('click', obtenerDatos);
@@ -7,3 +10,4 @@ function obtenerDatos() {
     var input = document.getElementById('miInput').value;
     console.log(input); // Muestra el valor del input en la consola del navegador
 }
+
